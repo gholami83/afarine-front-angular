c59a1dd11f0f3ec71df65d9b926de27ab7842c48
Hossein <hosseinama1383@gmail.com>
2024-02-03 13:56:35 +0330

    eventlist updated

--- src/app/features/eventlist/eventlist.component.ts
+++ src/app/features/eventlist/eventlist.component.ts
@@ -1,6 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { EventRoutingModule } from './event-routing.module';
-
 @Component({
   selector: 'app-eventlist',
   templateUrl: './eventlist.component.html',
@@ -8,4 +7,70 @@ import { EventRoutingModule } from './event-routing.module';
 })
 
 export class EventlistComponent {
+  events = [
+    {
+      'image':'assets/imgs/wp11572314.jpg',
+      'name':'کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp12154950.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp4700266.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp11572314.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp12154950.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp4700266.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp11572314.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+    {
+      'image':'assets/imgs/wp12154950.jpg',
+      'name':'رویداد کارآفرینی و کارگری',
+      'date':'سه شنبه ساعت 5 عصر',
+      'price':'5,000,000',
+      'location':'پاساژ زیتون',
+      'executer':'شرکت آفرینه',
+    },
+  ]
 }
