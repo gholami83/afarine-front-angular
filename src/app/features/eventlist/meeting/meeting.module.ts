209278f4059b334addfec0e746d759e9f23a4290
Hossein <hosseinama1383@gmail.com>
2024-02-07 17:29:53 +0330

    meeting updated

--- src/app/features/eventlist/meeting/meeting.module.ts
+++ src/app/features/eventlist/meeting/meeting.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MeetingRoutingModule } from './meeting-routing.module';
+import { MeetingComponent } from './meeting.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MeetingComponent],
   imports: [
     CommonModule,
     MeetingRoutingModule
