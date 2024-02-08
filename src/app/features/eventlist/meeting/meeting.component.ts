3b359866615a0dec810da6d0f8e59f6290e74413
Hossein <hosseinama1383@gmail.com>
2024-02-08 21:23:19 +0330

    meeting event updated

--- src/app/features/eventlist/meeting/meeting.component.ts
+++ src/app/features/eventlist/meeting/meeting.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./meeting.component.scss']
 })
 export class MeetingComponent {
-
+  public users = [
+    {
+      name: 'حسین غلامی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'سید عباس موسوی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'ابوالفضل زراعتکار',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'محمد‌امین طهماسبی‌نیا',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'مجتبی اکبری',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'مسعود آقایی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+  ]
+  public institutes = [
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+  ];
 }
--- src/app/features/eventlist/meeting/meeting.module.ts
+++ src/app/features/eventlist/meeting/meeting.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MeetingRoutingModule } from './meeting-routing.module';
 import { MeetingComponent } from './meeting.component';
+import { ShareModule } from 'src/app/shared/components/share/share.module';
 
 
 
@@ -9,6 +10,7 @@ import { MeetingComponent } from './meeting.component';
   declarations: [MeetingComponent],
   imports: [
     CommonModule,
+    ShareModule,
     MeetingRoutingModule
   ]
 })
