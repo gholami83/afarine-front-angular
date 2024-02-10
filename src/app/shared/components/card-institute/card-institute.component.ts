8998c025a8a098879efe4571779370649e453191
Hossein <hosseinama1383@gmail.com>
2024-02-10 18:44:54 +0330

    meeting & startup & cafe updated

--- src/app/shared/components/card-institute/card-institute.component.ts
+++ src/app/shared/components/card-institute/card-institute.component.ts
@@ -6,5 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card-institute.component.scss']
 })
 export class CardInstituteComponent {
-@Input() institute:any 
+@Input() institute:any;
+@Input() textstyle:any;
+@Input() bgstyle:string='';
 }
--- src/app/shared/components/card-user/card-user.component.ts
+++ src/app/shared/components/card-user/card-user.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-user',
   templateUrl: './card-user.component.html',
   styleUrls: ['./card-user.component.scss']
 })
-export class CardUserComponent {
-@Input() user:any;
-
+export class CardUserComponent{
+  @Input() user:any;
+  @Input() textstyle:string='';
+  @Input() rolestyle:string='';
+  @Input() bgstyle:string='';
 }
