00cc2760b586e03f678bcd086d23c00a1f6efe9c
Hossein <hosseinama1383@gmail.com>
2024-02-15 16:19:18 +0330

    workshop updated

--- src/app/features/eventlist/workshop/workshop.component.ts
+++ src/app/features/eventlist/workshop/workshop.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./workshop.component.scss']
 })
 export class WorkshopComponent {
-
+  public users = [
+    {
+      name: 'حسین غلامی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'سید عباس موسوی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'ابوالفضل زراعتکار',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'محمد‌امین طهماسبی‌نیا',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'مجتبی اکبری',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+    {
+      name: 'مسعود آقایی',
+      educate: 'کارشناسی کامپیوتر',
+      executive: 'مدیر ارشد هلدینگ آفرینه',
+      position: 'مجری دوره',
+    },
+  ]
+  public institutes = [
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+    {
+      name: 'شرکت آفرینه',
+    },
+  ];
 }
--- src/app/features/eventlist/workshop/workshop.module.ts
+++ src/app/features/eventlist/workshop/workshop.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkshoproutingModule } from './workshop-routing.module';
+import { WorkshopComponent } from './workshop.component';
+import { ShareModule } from 'src/app/shared/components/share/share.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WorkshopComponent],
   imports: [
     CommonModule,
+    ShareModule,
     WorkshoproutingModule
   ]
 })
