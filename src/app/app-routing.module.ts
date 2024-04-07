b33c96e795b52e4ab27002aee02166b16b1c835f
Hossein <hosseinama1383@gmail.com>
2024-04-07 16:14:54 +0330

    code updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NotfoundComponent } from './features/notfound/notfound.component';
 
 const routes: Routes = [
  { path: "", loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+ { path: "consult", loadChildren: () => import('./features/consult/consult.module').then(m => m.ConsultModule) },
  { path: "events", loadChildren: () => import('./features/eventlist/eventlist.module').then(m => m.EventlistModule) },
  { path: '**', redirectTo: '404' },
  { path: '404', component: NotfoundComponent }
